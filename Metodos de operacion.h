9f619431f8f5fafa3aea550736653e0ed7c835f4
Isaac Alfaro <103698346+NightmareG113@users.noreply.github.com>
2023-08-25 21:23:35 -0600

    Preliminar #1 caso 1 estructuras

---
+++ Metodos de operacion.h
@@ -0,0 +1,7 @@
+#pragma once
+
+metodos para agregar, eliminar, buscar, reordenar titulares;
+agregarElemento(const Elem& Elemento); // utiliza el metodo agregar de la lista, donde agrega un titular a la estructura
+eliminarElemento(const Elem& Elemento); // utiliza el metodo eliminar de la lista, donde elimina un titular de la estructura
+buscarElemento(const Elem& Elemento); // utiliza el metodo buscar de la lista, donde busca un titular especifico dentro de la estructura
+reordenarElemento(const Elem& Elemento); // utiliza el metodo ordenar de la lista, donde ordena un elemento de la estructura y lo cambia de posicion.
